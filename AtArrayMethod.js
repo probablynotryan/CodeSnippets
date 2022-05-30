a16093fa8a053a9d30d43860c5d46a539ef25aaa
Ryan Bass <theryanbass@gmail.com>
2022-05-29 21:02:46 -0700

    add AtArrayMethod

---
+++ AtArrayMethod.js
@@ -0,0 +1,6 @@
+let arr = ['amanda', 'bob', 'cindy', 'dylan', 'satan'];
+
+arr.at(0); // = 'amanda' (same as arr[0])
+arr.at(1); // = 'bob' (same as arr[1])
+arr.at(-1); // = 'satan' (same as arr[arr.length - 1])
+arr.at(-5); // = 'amanda'
