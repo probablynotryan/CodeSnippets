a3683af72790ecec09f4d55041f10aecb56ab73e
Ryan Bass <theryanbass@gmail.com>
2022-05-31 12:09:23 -0700

    move method demos into their own folder

---
+++ Methods/repeat.js
@@ -0,0 +1,4 @@
+//String.prototype.repeat();
+
+console.log("They call me Jane. " + "That's not my name. ".repeat(3));
+// They call me Jane. That's not my name. That's not my name. That's not my name.
